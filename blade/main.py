3fcea8b462454a5cdd59d578e4d4e7082a24c2e1
gaieepo <gaieepo@gmail.com>
2019-09-24 01:24:04 +0800

    weight init and self play support

--- blade/main.py
+++ blade/main.py
@@ -1,3 +1,4 @@
+import os
 import random
 from itertools import count
 
@@ -68,7 +69,7 @@ class Policy(nn.Module):
         return torch.softmax(action_scores, dim=1)
 
 
-def torch_agent(state, actions):
+def torch_agent(policy, state, actions):
     state = torch.from_numpy(state).float().unsqueeze(0)
     probs = policy(state)
     m = Categorical(probs)
@@ -83,21 +84,30 @@ def finish_episode():
     policy_loss = []
     returns = []
 
-    for r in policy.rewards[::-1]:
+    for r in latest_policy.rewards[::-1]:
         R = r + gamma * R  # discount reward
         returns.insert(0, R)
     returns = torch.tensor(returns)
     returns = (returns - returns.mean()) / (returns.std() + eps)
 
-    for log_prob, R in zip(policy.saved_log_probs, returns):
+    for log_prob, R in zip(latest_policy.saved_log_probs, returns):
         policy_loss.append(-log_prob * R)
     optimizer.zero_grad()
     policy_loss = torch.cat(policy_loss).sum()
     policy_loss.backward()
     optimizer.step()
-    del policy.rewards[:]
-    del policy.saved_log_probs[:]
+    del latest_policy.rewards[:]
+    del latest_policy.saved_log_probs[:]
+
     return policy_loss.item()
+
+
+def weight_init(m):
+    if isinstance(m, torch.nn.Conv2d) or isinstance(m, torch.nn.Linear):
+        torch.nn.init.xavier_uniform_(m.weight)
+
+        if m.bias:
+            torch.nn.init.xavier_uniform_(m.bias)
 
 
 if __name__ == "__main__":
@@ -114,20 +124,40 @@ if __name__ == "__main__":
     # naive torch agent settings
     render = False
     resume = False
+    updated = False
 
-    # policy model
-    policy = Policy(
+    # policy models
+    best_policy = Policy(
+        input_dim=state['white'].shape[0],
+        output_dim=len(game.available_actions),
+    )
+    latest_policy = Policy(
         input_dim=state['white'].shape[0],
         output_dim=len(game.available_actions),
     )
 
-    if resume:
-        policy.load_state_dict(torch.load('latest.pth'))
+    # misc
 
-    policy.train()
+    if resume:
+        if os.path.exists('latest.pth'):
+            latest_policy.load_state_dict(torch.load('latest.pth'))
+        elif os.path.exists('best.pth'):
+            latest_policy.load_state_dict(torch.load('best.pth'))
+        else:
+            weight_init(latest_policy)
+
+    if os.path.exists('best.pth'):
+        best_policy.load_state_dict(torch.load('best.pth'))
+    elif os.path.exists('best.pth'):
+        best_policy.load_state_dict(torch.load('latest.pth'))
+    else:
+        weight_init(best_policy)
+
+    latest_policy.train()
+    best_policy.eval()
 
     # hyper-parameters
-    optimizer = optim.Adam(policy.parameters(), lr=3e-4)
+    optimizer = optim.Adam(latest_policy.parameters(), lr=3e-4)
     eps = np.finfo(np.float32).eps.item()
     gamma = 0.99
 
@@ -139,12 +169,18 @@ if __name__ == "__main__":
         state, ep_reward = game.reset(), 0
         white_action, black_action = 'null', 'null'
 
+        if updated:
+            # elite best
+            updated = False
+            best_policy.load_state_dict(torch.load('best.pth'))
+
         for t in range(1, MAX_GLOBAL_TIME):  # finite loop while learning
             if render:
                 game.render(
                     white_action=white_action, black_action=black_action
                 )
 
+            # preprocess input state
             input_state = {
                 'white': np.concatenate([state['white'], prev_state['white']])
                 if prev_state['white'] is not None
@@ -159,18 +195,23 @@ if __name__ == "__main__":
             }
             prev_state = state
 
+            # generate actions
             white_action = torch_agent(
-                input_state['white'], game.available_actions
+                latest_policy, input_state['white'], game.available_actions
             )
-            black_action = random_agent(game.available_actions)
+            with torch.no_grad():
+                black_action = torch_agent(
+                    best_policy, input_state['black'], game.available_actions
+                )
 
             if white_action == 'close' or black_action == 'close':
                 game.close()
 
+            # update env
             state, reward, done, info = game.step(white_action, black_action)
 
             # record reward
-            policy.rewards.append(reward[WHITE])
+            latest_policy.rewards.append(reward[WHITE])
             ep_reward += reward[WHITE]
 
             if done:
@@ -178,21 +219,26 @@ if __name__ == "__main__":
 
         if not done:
             # when exceed time white loses
-            policy.rewards.append(-1)
+            latest_policy.rewards.append(-1)
             ep_reward += -1
 
         running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
-        policy_loss = finish_episode()
+        returned_policy_loss = finish_episode()
 
-        if reward[0] == 1:
+        if reward[WHITE] == 1:
             white_wins += 1
         else:
             black_wins += 1
+        white_win_rate = white_wins / (white_wins + black_wins)
 
         if i_episode % 50 == 0:
-            # torch.save(policy.state_dict(), f'save_{i_episode}.pth')
-            torch.save(policy.state_dict(), 'latest.pth')
+            torch.save(latest_policy.state_dict(), 'latest.pth')
+
+        if white_win_rate > 0.6:
+            updated = True
+            torch.save(latest_policy.state_dict(), 'best.pth')
+            print('Updated best policy!!!')
 
         print(
-            f'Ep: {i_episode:3d} ends at time {t:5d} loss: {policy_loss:.2f} reward: {ep_reward:.2f} avg reward: {running_reward:.2f} white: {white_wins} black: {black_wins} white rate: {100. * white_wins / (white_wins + black_wins):.2f}%'
+            f'Ep: {i_episode:3d} ends at time {t:5d} loss: {returned_policy_loss:.2f} reward: {ep_reward:.2f} avg reward: {running_reward:.2f} white: {white_wins} black: {black_wins} white rate: {100. * white_win_rate:.2f}%'
         )
