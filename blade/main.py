363d27fe005ea27af98ae31a93d6f656e919d4b4
gaieepo <gaieepo@gmail.com>
2019-10-07 10:43:12 +0800

    start migration of ppo

--- blade/main.py
+++ blade/main.py
@@ -1,267 +1,197 @@
-import os
-from itertools import count
+import multiprocessing
+import multiprocessing.connection
+import time
+from collections import deque
 
 import numpy as np
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from game import Game
 from torch.distributions import Categorical
-from utils import EPS, GAMMA, LR, WHITE
+
+from game import Game
+
+if torch.cuda.is_available():
+    device = torch.device('cuda:3')
+else:
+    device = torch.device('cpu')
 
 
-###################################################
-# PG related
-###################################################
-class Policy(nn.Module):
-    def __init__(self, input_dim, output_dim):
-        super(Policy, self).__init__()
-        self.fc1 = nn.Linear(input_dim * 2, 128)
-        self.dropout = nn.Dropout(p=0.6)
-        self.fc2 = nn.Linear(128, output_dim)
-
-        self.saved_log_probs = []
+class GameWrapper:
+    def __init__(self, seed):
+        self.env = Game()
+        self.obs_8 = np.zeros((8, 199))
         self.rewards = []
 
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.dropout(x)
-        x = F.relu(x)
-        action_scores = self.fc2(x)
+    def step(self, white_action, black_action):
+        obs, reward, done, info = self.env.step(white_action, black_action)
+        self.rewards.append(reward)
+        if done:
+            episode_info = {
+                'reward': sum(self.rewards),
+                'length': len(self.rewards),
+            }
+            self.reset()
+        else:
+            episode_info = None
+            self.obs_8 = np.roll(self.obs_8, shift=-1, axis=0)
+            self.obs_8[-1, :] = obs
 
-        return torch.softmax(action_scores, dim=1)
+        return self.obs_8.reshape(-1), reward, done, episode_info
 
-
-def torch_agent(policy, state, actions, save=True):
-    state = torch.from_numpy(state).float().unsqueeze(0)
-    probs = policy(state)
-    m = Categorical(probs)
-    action = m.sample()
-
-    if save:
-        best_policy.saved_log_probs.append(m.log_prob(action))
-
-    return actions[action.item()]
-
-
-def finish_episode():
-    R = 0
-    policy_loss = []
-    returns = []
-
-    for r in best_policy.rewards[::-1]:
-        R = r + GAMMA * R  # discount reward
-        returns.insert(0, R)
-    returns = torch.tensor(returns)
-    returns = (returns - returns.mean()) / (returns.std() + EPS)
-
-    for log_prob, R in zip(best_policy.saved_log_probs, returns):
-        policy_loss.append(-log_prob * R)
-    optimizer.zero_grad()
-    policy_loss = torch.cat(policy_loss).sum()
-    policy_loss.backward()
-    optimizer.step()
-    del best_policy.rewards[:]
-    del best_policy.saved_log_probs[:]
-
-    return policy_loss.item()
+    def reset(self):
+        obs = self.env.reset()
+        self.obs_8[0, :] = obs
+        self.obs_8[1, :] = obs
+        self.obs_8[2, :] = obs
+        self.obs_8[3, :] = obs
+        self.obs_8[4, :] = obs
+        self.obs_8[5, :] = obs
+        self.obs_8[6, :] = obs
+        self.obs_8[7, :] = obs
+        self.rewards = []
+        return self.obs_8.reshape(-1)
 
 
-def weight_init(m):
-    if isinstance(m, torch.nn.Conv2d) or isinstance(m, torch.nn.Linear):
-        torch.nn.init.xavier_uniform_(m.weight)
+def worker_process(remote, seed):
+    env = GameWrapper(seed)
+    while True:
+        cmd, data = remote.recv()
+        if cmd == 'step':
+            remote.send(env.step(data))
+        elif cmd == 'reset':
+            remote.send(env.reset())
+        elif cmd == 'close':
+            remote.close()
+            break
+        else:
+            raise NotImplementedError
 
-        if m.bias:
-            torch.nn.init.xavier_uniform_(m.bias)
+
+class Worker:
+    def __init__(self, seed):
+        self.child, parent = multiprocessing.Pipe()
+        self.process = multiprocessing.Process(
+            target=worker_process, args=(parent, seed)
+        )
+        self.process.start()
+
+
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(199 * 8, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 128)
+        self.pi_logits = nn.Linear(128, 17)
+        self.value = nn.Linear(128, 1)
+
+    def forward(self, obs):
+        h = F.relu(self.fc1(obs))
+        h = F.relu(self.fc2(h))
+        h = F.relu(self.fc3(h))
+
+        pi = Categorical(logits=self.pi_logits(h))
+        value = self.value(h).reshape(-1)
+
+        return pi, value
+
+
+def obs_to_torch(obs):
+    return torch.tensor(obs, dtype=torch.float32, device=device)
+
+
+class Trainer:
+    def __init__(self, model):
+        self.model = model
+        self.optimizer = optim.Adam(self.model.parameters(), lr=2.5e-4)
+
+    def train(self, samples, learning_rate, clip_range):
+        pass
+
+
+class Main:
+    def __init__(self):
+        self.gamma = 0.99
+        self.lamda = 0.95
+        self.updates = 10000
+        self.epochs = 4
+        self.n_workers = 8
+        self.worker_steps = 128
+        self.n_mini_batch = 4
+        self.batch_size = self.n_workers * self.worker_steps  # 8 * 128 = 1024
+        self.mini_batch_size = (
+            self.batch_size // self.n_mini_batch
+        )  # 8 * 128 // 4 = 256
+        assert self.batch_size % self.n_mini_batch == 0
+
+        self.workers = [Worker(42 + i) for i in range(self.n_workers)]
+
+        self.obs = np.zeros((self.n_workers, 8, 199), dtype=np.uint8)
+
+        for worker in self.workers:
+            worker.child.send(('reset', None))
+        for i, worker in enumerate(self.workers):
+            self.obs[i] = worker.child.recv()
+
+        self.model = Model()
+        self.model.to(device)
+        self.trainer = Trainer(self.model)
+
+    def sample(self):
+        pass
+
+    def _calc_advantages(self, dones, rewards, values):
+        pass
+
+    def train(self, samples, learning_rate, clip_range):
+        train_info = []
+
+        for _ in range(self.epochs):
+            indexes = torch.randperm(self.batch_size)
+
+    def run_training_loop(self):
+        episode_info = deque(maxlen=100)
+
+        for update in range(self.updates):
+            time_start = time.time()
+
+            # lr schedule
+            progress = update / self.updates
+            learning_rate = 2.5e-4 * (1 - progress)
+            clip_range = 0.1 * (1 - progress)
+
+            samples, sample_episode_info = self.sample()
+            self.train(samples, learning_rate, clip_range)
+
+            time_end = time.time()
+
+            fps = int(self.batch_size / (time_end - time_start))
+            episode_info.extend(sample_episode_info)
+            reward_mean, length_mean = Main._get_mean_episode_info(
+                episode_info
+            )
+            print(
+                f'{update:4}: fps={fps:3} reward={reward_mean:.2f} length={length_mean:.3f}'
+            )
+
+    @staticmethod
+    def _get_mean_episode_info(episode_info):
+        if len(episode_info) > 0:
+            return (
+                np.mean([info['reward'] for info in episode_info]),
+                np.mean([info['length'] for info in episode_info]),
+            )
+        else:
+            return np.nan, np.nan
+
+    def destroy(self):
+        for worker in self.workers:
+            worker.child.send(('close', None))
 
 
 if __name__ == "__main__":
-    """ Self-play training procedure """
-    # env settings (should not use seeded random during training)
-    # random.seed(SEED)
-    # torch.manual_seed(SEED)
-
-    # env setup
-    game = Game()
-    state = game.reset()
-
-    # policy models
-    best_policy = Policy(
-        input_dim=state['white'].shape[0],
-        output_dim=len(game.available_actions),
-    )
-    latest_policy = Policy(
-        input_dim=state['white'].shape[0],
-        output_dim=len(game.available_actions),
-    )
-
-    if os.path.exists('best.pth'):  # load best
-        latest_policy.load_state_dict(torch.load('best.pth'))
-        best_policy.load_state_dict(torch.load('best.pth'))
-    else:
-        weight_init(latest_policy)
-        weight_init(best_policy)
-        torch.save(best_policy.state_dict(), 'best.pth')
-
-    # hyper-parameters
-    optimizer = optim.Adam(best_policy.parameters(), lr=LR)
-
-    # main loop
-    episode_number = 0
-
-    while True:
-        # 1. self-play 1000 games
-        print('Start self-play!!!')
-        latest_policy.eval()
-        best_policy.train()
-
-        white_wins, black_wins = 0, 0
-
-        for i_selfplay_episode in range(1, 1001):
-            prev_state = {'white': None, 'black': None}
-            state = game.reset()
-
-            # load best model
-            latest_policy.load_state_dict(torch.load('best.pth'))
-            best_policy.load_state_dict(torch.load('best.pth'))
-
-            for t in count(1):  # TODO unlimited game time ???
-                # preprocess input state
-                input_state = {
-                    'white': np.concatenate(
-                        [state['white'], prev_state['white']]
-                    )
-                    if prev_state['white'] is not None
-                    else np.concatenate(
-                        [state['white'], np.zeros_like(state['white'])]
-                    ),
-                    'black': np.concatenate(
-                        [state['black'], prev_state['black']]
-                    )
-                    if prev_state['black'] is not None
-                    else np.concatenate(
-                        [state['black'], np.zeros_like(state['black'])]
-                    ),
-                }
-                prev_state = state
-
-                # generate actions
-                white_action = torch_agent(
-                    best_policy, input_state['white'], game.available_actions
-                )
-                black_action = torch_agent(
-                    best_policy,
-                    input_state['black'],
-                    game.available_actions,
-                    save=False,
-                )
-
-                # update env
-                state, reward, done, info = game.step(
-                    white_action, black_action
-                )
-
-                # record reward
-                best_policy.rewards.append(reward[WHITE])
-
-                if done:
-                    returned_policy_loss = finish_episode()
-
-                    if reward[WHITE] == 1:
-                        white_wins += 1
-                    else:
-                        black_wins += 1
-                    selfplay_white_win_rate = white_wins / (
-                        white_wins + black_wins
-                    )
-
-                    episode_number += 1
-
-                    print(
-                        f'{episode_number} [{i_selfplay_episode}/1000-{t}] loss: {returned_policy_loss:.2f} white: {white_wins} black: {black_wins} white rate: {100. * selfplay_white_win_rate:.2f}%'
-                    )
-
-                    # IMPORTANT copy weight from best to latest
-                    latest_policy.load_state_dict(best_policy.state_dict())
-
-                    break
-
-        # 2. 400 matches between latest and best
-        print('Start matches!!!')
-        best_policy.eval()
-
-        white_wins, black_wins = 0, 0
-        match_white_win_rate = 0.0
-
-        # load best model
-        best_policy.load_state_dict(torch.load('best.pth'))
-
-        for i_match_episode in range(1, 401):
-            prev_state = {'white': None, 'black': None}
-            state = game.reset()
-
-            for t in count(1):
-                # preprocess input state
-                input_state = {
-                    'white': np.concatenate(
-                        [state['white'], prev_state['white']]
-                    )
-                    if prev_state['white'] is not None
-                    else np.concatenate(
-                        [state['white'], np.zeros_like(state['white'])]
-                    ),
-                    'black': np.concatenate(
-                        [state['black'], prev_state['black']]
-                    )
-                    if prev_state['black'] is not None
-                    else np.concatenate(
-                        [state['black'], np.zeros_like(state['black'])]
-                    ),
-                }
-                prev_state = state
-
-                # generate actions
-                with torch.no_grad():
-                    white_action = torch_agent(
-                        latest_policy,
-                        input_state['white'],
-                        game.available_actions,
-                        save=False,
-                    )
-                    black_action = torch_agent(
-                        best_policy,
-                        input_state['black'],
-                        game.available_actions,
-                        save=False,
-                    )
-
-                # update env
-                state, reward, done, info = game.step(
-                    white_action, black_action
-                )
-
-                # record reward
-                latest_policy.rewards.append(reward[WHITE])
-
-                if done:
-                    if reward[WHITE] == 1:
-                        white_wins += 1
-                    else:
-                        black_wins += 1
-                    match_white_win_rate = white_wins / (
-                        white_wins + black_wins
-                    )
-
-                    print(
-                        f'[{i_match_episode}/400-{t}] white: {white_wins} black: {black_wins} white rate: {100. * match_white_win_rate:.2f}%'
-                    )
-
-                    break
-
-        if match_white_win_rate > 0.55:
-            torch.save(latest_policy.state_dict(), 'best.pth')
-            print(
-                f'White win rate is {match_white_win_rate:.2f} above 55%, saved new best'
-            )
+    m = Main()
+    m.run_training_loop()
+    m.destroy()
